af5fb85e2ef31ab8141bae58fb77bdd3867bcfce
mohityadav24 <112639626+mohityadav24@users.noreply.github.com>
2025-12-11 10:42:48 +0530

    Remove debug console log from passport.js
    Remove console log for refresh token in passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -15,7 +15,7 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ googleId: profile.id });
-console.log("test google token ",refreshToken);
+
 
         if (!user) {
           user = await User.create({
